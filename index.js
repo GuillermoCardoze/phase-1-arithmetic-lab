6c7be9c239ce860700bb4a1c81641eecc55cd728
Guillermo Cardoze <vital_tmv@yahoo.com>
2023-10-30 18:20:49 -0500

    complete

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2;
+ 
+
+const random = Math.ceil(Math.random() * 100);
+
+
+const num3 = 15;
+const num4 = 11;
+
+const mod = num3 % num4;
+
+const numbers = [10, 5, 20, 15];
+const max = Math.max(...numbers);
